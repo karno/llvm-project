1a40520b7e2e79c87c85c1f4cf7e5ecb1066d23b
Karno <karnoroid@gmail.com>
2022-08-28 17:44:55 +0900

    update comment.

--- clang/examples/LoopRewriter/LoopRewriter.cpp
+++ clang/examples/LoopRewriter/LoopRewriter.cpp
@@ -1,13 +1,14 @@
 //===- LoopRewriter.cpp ---------------------------------------------===//
 //
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// This source code is originally part of the LLVM Project,
+// under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
 //
-// Example clang plugin which simply prints the names of all the top-level decls
-// in the input file.
+// Example clang plugin with modification of AST to add `break`
+// into the `for` loop.
 //
 //===----------------------------------------------------------------------===//
 
